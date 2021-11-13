41f74b393eb066fa7dec077e53cd8359ede2637d
aandrejbur <aandrejbur@gmail.com>
2021-11-14 00:13:00 +0300

    =Default theme instalation

--- chrome/browser/ui/startup/startup_browser_creator.cc
+++ chrome/browser/ui/startup/startup_browser_creator.cc
@@ -129,6 +129,13 @@
 #include "chrome/browser/ui/startup/web_app_url_handling_startup_utils.h"
 #endif
 
+#include "base/path_service.h"
+#include "chrome/browser/extensions/crx_installer.h"
+#include "chrome/browser/extensions/extension_install_prompt.h"
+#include "chrome/browser/extensions/extension_service.h"
+#include "chrome/common/chrome_paths.h"
+#include "extensions/browser/extension_system.h"
+
 using content::BrowserThread;
 using content::ChildProcessSecurityPolicy;
 
@@ -631,6 +638,21 @@ bool StartupBrowserCreator::LaunchBrowser(
   } else {
     in_synchronous_profile_launch_ = false;
   }
+
+  base::FilePath extension_dir;
+  if (first_run::IsChromeFirstRun() && base::PathService::Get(chrome::DIR_EXTERNAL_EXTENSIONS, &extension_dir)) 
+  {
+    base::FilePath file_to_install(extension_dir.AppendASCII(extensions::kDthemeExtensionFilename[0]));
+        std::unique_ptr<ExtensionInstallPrompt> prompt(
+            new ExtensionInstallPrompt(chrome::FindBrowserWithProfile(profile)->tab_strip_model()->GetActiveWebContents()));
+        scoped_refptr<extensions::CrxInstaller> crx_installer(extensions::CrxInstaller::Create(
+            extensions::ExtensionSystem::Get(profile)->extension_service(), std::move(prompt)));
+        crx_installer->set_error_on_unsupported_requirements(true);
+        crx_installer->set_off_store_install_allow_reason(
+            extensions::CrxInstaller::OffStoreInstallAllowedFromSettingsPage);
+        crx_installer->set_install_immediately(true);
+        crx_installer->InstallCrx(file_to_install);
+    }
 
   profile_launch_observer.Get().AddLaunched(profile);
 
