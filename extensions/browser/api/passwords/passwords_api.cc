5e26f071d4d22c98cbe5395f0f6a9e2ade36270b
Valeriy <tor-tech@mail.ru>
2022-06-12 02:37:01 +0300

    add password function works but not all params is added

--- extensions/browser/api/passwords/passwords_api.cc
+++ extensions/browser/api/passwords/passwords_api.cc
@@ -1,7 +1,8 @@
 #include "extensions/browser/api/passwords/passwords_api.h"
 #include "out/release/gen/extensions/common/api/passwords.h"
 #include <memory>
-
+#include <codecvt>
+#include <locale>
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/password_manager/password_store_factory.h"
 #include "components/password_manager/core/browser/ui/saved_passwords_presenter.h"
@@ -15,7 +16,7 @@ ExtensionFunction::ResponseAction PasswordsGetFunction::Run() {
    EXTENSION_FUNCTION_VALIDATE(params.get());
 
   base::Value dict(base::Value::Type::DICTIONARY);
-  dict.SetStringKey("decentr_storage_key", "decentr_storage_data");
+  dict.SetStringKey("key", "fsfsfsdfsdfsd");
   return RespondNow(OneArgument(base::Value(std::move(dict))));
 }
 
@@ -24,16 +25,22 @@ ExtensionFunction::ResponseAction PasswordsAddFunction::Run() {
   std::unique_ptr<passwords::Add::Params> params(
   passwords::Add::Params::Create(args()));
   EXTENSION_FUNCTION_VALIDATE(params.get());
-  //LOG(INFO) << "Passwords params getting";
+  LOG(INFO) << params->data.password;
   Profile* profile = Profile::FromBrowserContext(browser_context());
   scoped_refptr<password_manager::PasswordStoreInterface> store =
-  PasswordStoreFactory::GetForProfile(profile,
-                                          ServiceAccessType::EXPLICIT_ACCESS);
+  PasswordStoreFactory::GetForProfile(profile, ServiceAccessType::EXPLICIT_ACCESS);
   password_manager::SavedPasswordsPresenter spp(store);
   spp.Init();
+ 
+  std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
+  std::string password = params->data.password;
+  std::u16string password_value = convert.from_bytes(password);
+  std::string username = params->data.username;
+  std::u16string username_value = convert.from_bytes(username);
+
   password_manager::PasswordForm password_form;
-  password_form.username_value = u"SuperTestUser4";
-  password_form.password_value = u"SuperPassword4";
+  password_form.username_value = username_value;
+  password_form.password_value = password_value;
   password_form.url = GURL("https://account.mail.ru/login/");
   password_form.action = GURL("https://auth.mail.ru/cgi-bin/auth");
   password_form.signon_realm = "https://account.mail.ru/";
